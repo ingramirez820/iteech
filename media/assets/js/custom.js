47dc6477792f299edb831798edefd545227ed258
ingramirez820 <ingramirez820@gmail.com>
2021-01-29 01:17:32 -0600

    Eliminando iconos del footer y corrigiendo detalles

--- media/assets/js/custom.js
+++ media/assets/js/custom.js
@@ -39,16 +39,54 @@ var laLinea = $("#laLinea");
       headerMain.css({
         "background": "rgba(254, 254, 254, .9)"});
       $(".navbar").css("background","rgba(163, 0, 81, 1)");
-      $(".logo-DG").css("width", "200px");
+      $(".logo-DG").css("width", "60px");
     } else {
       headerMain.css(
         {
         "background": "rgba(254, 254, 254, 1)"});
         $(".navbar").css("background","rgba(163, 0, 81, 1)");
-        $(".logo-DG").css("width", "300px");
+        $(".logo-DG").css("width", "147px");
     }
   });
 
+/*   // Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+} */
 // Ejemplo para el modal
 var cardPopUp = $("#card-popUp");
 var mainContent = $(".main");
